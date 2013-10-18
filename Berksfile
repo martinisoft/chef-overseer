b10378688b4b0ef192cb66ce389aaa9823b9e61f
Aaron Kalin <akalin@martinisoftware.com>
2013-10-17 20:51:13 -0500

    Update Berksfile and metadata

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+cookbook 'rvm', github: 'fnichol/chef-rvm', ref: 'master'
+
+metadata
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "overseer"
 maintainer       "Aaron Kalin"
 maintainer_email "akalin@martinisoftware.com"
 license          "Apache 2.0"
 description      "Opinionated web application deployment."
-long_description "Please refer to README.md"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.11"
 
 supports "ubuntu"
